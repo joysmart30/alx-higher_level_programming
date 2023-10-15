09a7fb4c825842af580f2c4c23938dbfccfe5f98
Joy Emeh <joysmart30@gmail.com>
2023-10-15 16:57:24 -0700

    unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    # get elements common to the two sets (intersection)
-    common = set_1.intersection(set_2)
-    return common
+def uniq_add(my_list=[]):
+    # Create a set from the list to get only unique elements
+    unique_list = set(my_list)
+
+    # Initialize a variable to hold the sum
+    sum = 0
+
+    # Iterate over the unique elements and add them to the sum
+    for item in unique_list:
+        sum += item
+
+    # Return the sum
+    return sum
